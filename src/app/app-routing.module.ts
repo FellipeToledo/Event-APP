ec0d3b1cae2498ee9b75f77428e86ed9a19735a6
Fellipe <fellipetoledo.dev@gmail.com>
2022-06-20 15:30:58 -0300

    Creating a Event Module and Using Routing with Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ' ' , pathMatch: 'full',  redirectTo: 'events'},
+  {
+    path: 'events',
+    loadChildren: () => import('./events/events.module').then(m => m.EventsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
