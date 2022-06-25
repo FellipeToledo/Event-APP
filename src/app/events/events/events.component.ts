36577c86328d3c6df9314cf40a6195c0fea867c7
Fellipe <fellipetoledo.dev@gmail.com>
2022-06-25 15:24:23 -0300

    Implemented http get call

--- src/app/events/events/events.component.ts
+++ src/app/events/events/events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Event } from '../model/event';
 import { EventService } from '../service/event.service';
@@ -10,12 +11,7 @@ import { EventService } from '../service/event.service';
 })
 export class EventsComponent implements OnInit {
 
-  events: Event[] = [
-  {_id: 10, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 15, 50),  location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-  {_id: 20, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 16, 50), location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-  {_id: 30, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 16, 50), location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-  {_id: 40, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 16, 50), location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-  ];
+  events: Observable<Event[]>;
   displayedColumns = ['_id', 'source', /*'localDateTime',*/ 'startingDateTime', 'endDateTime' , 'location', 'description', 'blockedRoads', 'impactedLines'];
 
    //eventService: EventService;
--- src/app/events/service/event.service.ts
+++ src/app/events/service/event.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
+  private readonly API = '/assets/events.json';
+
 constructor(private httpClient: HttpClient) { }
 
-list(){
-  return [
-    {_id: 10, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 15, 50),  location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-    {_id: 20, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 16, 50), location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-    {_id: 30, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 16, 50), location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-    {_id: 40, source: 'COR', localDateTime: new Date(), startingDateTime: new Date(2022, 6, 26, 13, 42), endDateTime: new Date(2022, 6, 26, 16, 50), location: 'Avenida Presidente Vargas', description: 'Queda de poste', blockedRoads: 'Vargas', impactedLines: '774'},
-    ];
+list(): any{
+  return this.httpClient.get<Event[]>(this.API)
+  .pipe(
+    first(),
+    tap(events =>console.log(events))
+  );
 }
 
 }
